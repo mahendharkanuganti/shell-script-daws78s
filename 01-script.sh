e3225c80e6b8bcc4a5cad4438a246c445921f0c1
mahidarling <kanuganti98@gmail.com>
2024-04-18 14:11:43 +0530

    new content added

--- 01-script.sh
+++ 01-script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Ramesh: Hello suresh"
-echo "suresh: hello ramesh"
-echo "ramesh: how are you"
-echo "suresh: Iam good how are you"
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: Hello $PERSON2"
+echo "$PERSON2: hello $PERSON1"
+echo "$PERSON1: how are you"
+echo "$PERSON2: Iam good how are you"

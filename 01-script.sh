3c01b0297b02714500ab8ace8c03621253caaa2d
mahidarling <kanuganti98@gmail.com>
2024-04-18 14:26:20 +0530

    new content

--- 01-script.sh
+++ 01-script.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-No1=$1
-No2=$2
-
-SUM=$(($No1+$No2))
-
-echo "The sum of No1 and No2 is: $SUM"
+echo "All variables: $@"
+echo "Number of variables passed: $#"
+echo "Script Name: $0"
+echo "Current working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
+echo "Hostname: $HOSTNAME"
+echo "Process ID of the current shell script: $$"
+sleep 60 &
+echo "Process ID of last background command: $!"
+echo "Exit code: $?"

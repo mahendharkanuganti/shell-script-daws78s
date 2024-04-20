1302be18a6f57f896c303c7fba0e6b0746c839cc
mahidarling <kanuganti98@gmail.com>
2024-04-20 14:35:51 +0530

    updated changes

--- backend.sh
+++ backend.sh
@@ -6,7 +6,7 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE(){
-    if [ $1 -ne 0]
+    if [ $1 -ne 0 ]
     then
         echo -e "$2.... FAILURE"
         exit 1
